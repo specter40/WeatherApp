87c4bdcfa62b2221f24f885be268361e3618c030
Shaun Joseph <skj92852@uga.edu>
2023-05-01 10:53:41 -0400

    Started Implementing HTTP requests for 1st API

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -13,6 +13,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Priority;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+import javafx.stage.Modality;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 
 import java.io.IOException;
@@ -46,47 +55,168 @@ public class ApiApp extends Application {
         .setPrettyPrinting()                          // enable nice output when printing
         .create();                                    // builds and returns a Gson object
 
-
-
+    private static final CITY_KEY;
+    private static final String CITY_API = "https://api.api-ninjas.com/v1/city";
 
     Stage stage;
     Scene scene;
     VBox root;
+
+    //Top row
+    private Label search;
+    private Button getForcast;
+    private HBox urlBox;
+    private Button getCity;
+
+    private TextField url;
+    private boolean player;
+    private String uri;
+
+    //2nd row
+    private HBox texter;
+    private Label text;
+
+    //Text
+    private TextFlow body;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
     public ApiApp() {
+        //first row
         root = new VBox();
+        getForcast = new Button("Get Forcast");
+        this.search = new Label("Search:");
+        Font sizer = new Font(14);
+        this.search.setFont(sizer);
+        this.uri = "";
+        this.urlBox = new HBox(4);
+        getCity = new Button("Get City");
+        this.url = new TextField();
+        //2nd row initialized
+        this.text = new Label("Type in a city and click the button.");
+        this.texter = new HBox(8);
+
+        //Text
+        this.body
+
     } // ApiApp
 
+
+    /** {@inheritDoc} */
+    @Override
+    public void init() {
+        root.getChildren().addAll(urlBox, texter, body);
+        Insets margin = new Insets(4, 4, 4, 4);
+        //first row
+        VBox.setMargin(urlBox, margin);
+        VBox.setMargin(texter, margin);
+        urlBox.getChildren().addAll(getForcast, search, url, getCity);
+        HBox.setHgrow(url, Priority.ALWAYS);
+        //second row
+        texter.getChildren().add(text);
+
+
+        //body
+
+    }
     /** {@inheritDoc} */
     @Override
     public void start(Stage stage) {
 
         this.stage = stage;
 
-        // demonstrate how to load local asset using "file:resources/"
-        Image bannerImage = new Image("file:resources/readme-banner.png");
-        ImageView banner = new ImageView(bannerImage);
-        banner.setPreserveRatio(true);
-        banner.setFitWidth(640);
-
-        // some labels to display information
-        Label notice = new Label("Modify the starter code to suit your needs.");
-
         // setup scene
-        root.getChildren().addAll(banner, notice);
         scene = new Scene(root);
-
         // setup stage
-        stage.setTitle("ApiApp!");
+        stage.setTitle("City Weather!");
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> Platform.exit());
         stage.sizeToScene();
         stage.show();
 
     } // start
+
+    private void getCity() {
+        String configPath = "resources/config.properties";
+        // the following try-statement is called a try-with-resources statement
+        // see https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+        try (FileInputStream configFileStream = new FileInputStream(configPath)) {
+            Properties config = new Properties();
+            config.load(configFileStream);
+            config.list(System.out);                                  // list all using standard out
+            CITY_KEY = config.getProperty("apininja.apikey");
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+            ioe.printStackTrace();
+        } // try
+
+        try {
+            this.getForcast.setDisable(true);
+            this.getCity.setDisable(true);
+            Platform.runLater(() -> text.setText("Getting City..."));
+            // form URI
+            String name = URLEncoder.encode(this.url.getText(), StandardCharsets.UTF_8);
+            String query = String.format("?name=%s", name);
+            this.uri = CITY_API + query;
+            System.out.println(uri);
+            //build request
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("X-Our-Header-1", CITY_KEY)
+                .build();
+            // send request / receive response in the form of a String
+            HttpResponse<String> response = HTTP_CLIENT
+                .send(request, BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new IOException(response.toString());
+            } // if
+            // get request body (the content we requested)
+            String jsonString = response.body();
+            // parse the JSON-formatted string using GSON
+            ItunesResponse itunesResponse = GSON
+                .fromJson(jsonString, cs1302.gallery.ItunesResponse.class);
+
+
+
+            printItunesResponse(itunesResponse);
+            this.getForcast.setDisable(false);
+            this.getCity.setDisable(false);
+            Platform.runLater(() -> text.setText(uri));
+        } catch (IOException | InterruptedException | IllegalArgumentException e) {
+            // either:
+            // 1. an I/O error occurred when sending or receiving;
+            // 2. the operation was interrupted; or
+            // 3. the Image class could not load the image.
+            if (imageArray[0].getImage().getUrl().equals("file:" + DEFAULT_IMG)) {
+                this.getImages.setDisable(false);
+            } else {
+                this.play.setDisable(false);
+                this.getImages.setDisable(false);
+            }
+            Platform.runLater(() -> {
+                text.setText("Last attempt to get images failed...");
+                this.loadBar.setProgress(1);
+                alertError(e, this.uri);
+            });
+        } // try
+
+
+    }
+
+    /**
+     * Creates and immediately starts a new daemon thread that executes
+     * {@code target.run()}. This method, which may be called from any thread,
+     * will return immediately its the caller.
+     * @param target the object whose {@code run} method is invoked when this
+     *               thread is started
+     */
+    private void runNow(Runnable target) {
+        Thread t = new Thread(target);
+        t.setDaemon(true);
+        t.start();
+    } // runNow
+
 
 } // ApiApp
